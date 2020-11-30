68c91dcc4a62382ce45f58da4ad2a52bd113b7e7
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-11-29 21:43:30 -0600

    user-routes created

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,28 @@
 const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/user-controller');
+
+// Set up GET all and POST at /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// ****need to set up post and delete for friends*****
+
+mondule.exports = router;
