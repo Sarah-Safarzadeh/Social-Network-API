947a7350b2c8a7a9b5a15c7701ae545445363022
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-11-29 21:58:13 -0600

    added reaction routes, all routes for thoughts

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,30 @@
 const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+// GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// GET one, PUT, and DELETE at /api/thoughts/<userId>
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// Reactions
+router.route('/:id/reactions').post(createReaction);
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
